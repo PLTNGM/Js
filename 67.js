bc8e3d8497a07d96b3465d1e54d1bc8d59ffbe70
PLTNGM <platongimp96@gmail.com>
2025-11-25 12:43:40 +0500

    Add files via upload

---
+++ 67.js
@@ -0,0 +1,40 @@
+'use strict'
+
+// 1
+let r = 5; // радиус круга
+let s = Math.PI * r * r; // формула площади круга: π * r²
+console.log(s);
+
+
+// 2
+let a = 5;
+let s1 = a * a;
+console.log(s1);
+
+
+// 3
+let a1 = 5;
+let b1 = 10;
+let s2 = a1 * b1;
+console.log(s2);
+
+
+// 4
+let a2 = 5;
+let b2 = 10;
+let s3 = 2*(a2 + b2);
+console.log(s3);
+
+
+
+// 5
+let tk = 10
+let f = (tk * 1.8) + 32
+console.log(f);
+
+
+// 6
+let tf = 50
+let tkr = (tf - 32) / 1.8
+console.log(tkr)
+
